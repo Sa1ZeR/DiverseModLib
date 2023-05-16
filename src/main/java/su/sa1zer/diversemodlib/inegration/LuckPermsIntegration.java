6551ceef3ea41bba40e5500afe3b609708f702fd
Serg1K <amsher2007@mail.ru>
2023-05-16 20:19:09 +1000

    group check

--- src/main/java/su/sa1zer/diversemodlib/inegration/LuckPermsIntegration.java
+++ src/main/java/su/sa1zer/diversemodlib/inegration/LuckPermsIntegration.java
@@ -2,8 +2,13 @@ package su.sa1zer.diversemodlib.inegration;
 
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.NodeType;
+import net.luckperms.api.node.types.InheritanceNode;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.level.ServerPlayer;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LuckPermsIntegration {
 
@@ -18,5 +23,17 @@ public class LuckPermsIntegration {
         User user = LuckPermsProvider.get().getPlayerAdapter(ServerPlayer.class).getUser(player);
 
         return user.getCachedData().getPermissionData().checkPermission(permission).asBoolean();
+    }
+
+    public static boolean hasGroup(ServerPlayer player, String group) {
+        if (player == null)
+            return false;
+
+        User user = LuckPermsProvider.get().getPlayerAdapter(ServerPlayer.class).getUser(player);
+
+        Set<String> groups = user.getNodes(NodeType.INHERITANCE).stream()
+                .map(InheritanceNode::getGroupName)
+                .collect(Collectors.toSet());
+        return groups.contains(group);
     }
 }
