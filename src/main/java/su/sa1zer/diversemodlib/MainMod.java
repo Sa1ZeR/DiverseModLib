2983ae370089510fc9e9bf4b251eed65be7fba5f
Serg1K <amsher2007@mail.ru>
2023-05-20 16:04:32 +1000

    give items

--- src/main/java/su/sa1zer/diversemodlib/MainMod.java
+++ src/main/java/su/sa1zer/diversemodlib/MainMod.java
@@ -3,6 +3,7 @@ package su.sa1zer.diversemodlib;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import su.sa1zer.diversemodlib.config.LangResource;
 
 @Mod(MainMod.MOD_ID)
 public class MainMod {
@@ -11,7 +12,9 @@ public class MainMod {
 
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-    public MainMod() {
+    public static LangResource langResource;
 
+    public MainMod() {
+        langResource = new LangResource("ru_ru", this.getClass(), MOD_ID);
     }
 }
--- src/main/java/su/sa1zer/diversemodlib/utils/PlayerUtils.java
+++ src/main/java/su/sa1zer/diversemodlib/utils/PlayerUtils.java
@@ -5,10 +5,16 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import su.sa1zer.diversemodlib.MainMod;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @UtilityClass
@@ -42,5 +48,55 @@ public class PlayerUtils {
         int lvl = player.experienceLevel;
         player.teleportTo(world, x, y, z, player.getYRot(), player.getXRot());
         player.setExperienceLevels(lvl);
+    }
+
+    public void giveItemsSafely(Player player, List<ItemStack> items) {
+//        List<ItemStack> overSize = new ArrayList<>();
+
+        int freeSlotsAmount = getFreeSlotsAmount(player);
+        int i;
+        for(i = 0; i < freeSlotsAmount; i++) {
+            ItemStack itemStack = items.get(i);
+//            if(itemStack.getCount() > 64) {
+//                ItemStack over = itemStack.copy();
+//                int size = itemStack.getCount() - 64;
+//                over.setCount(size);
+//                overSize.add(over);
+//
+//                itemStack.setCount(itemStack.getCount() - size);
+//            }
+
+            player.addItem(itemStack);
+        }
+
+        boolean isWarn = i < items.size();
+        for(;i < items.size(); i++) {
+            ItemStack itemStack = items.get(i);
+
+            BlockPos pos = player.blockPosition();
+            player.level.addFreshEntity(new ItemEntity(player.level, pos.getX(), pos.getY() + 1, pos.getZ(), itemStack));
+        }
+
+        if(isWarn) {
+            MessageUtils.sendChatMessage(player,
+                    MainMod.langResource.getMessage("full-inventory-waring"));
+        }
+    }
+
+    public static boolean isFullInventory(Player player) {
+        for(int i = 0; i < player.getInventory().items.size(); i++) {
+            ItemStack itemStack = player.getInventory().getItem(i);
+            if(itemStack.equals(ItemStack.EMPTY)) return false;
+        }
+        return true;
+    }
+
+    public static int getFreeSlotsAmount(Player player) {
+        int count = 0;
+        for(int i = 0; i < player.getInventory().items.size(); i++) {
+            ItemStack itemStack = player.getInventory().getItem(i);
+            if(itemStack.equals(ItemStack.EMPTY)) count++;
+        }
+        return count;
     }
 }
