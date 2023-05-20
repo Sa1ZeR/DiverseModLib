bdd99292a89990701a8809e7c9aebe29a6bfd08d
Serg1K <amsher2007@mail.ru>
2023-05-20 17:36:43 +1000

    time utils

---
+++ src/main/java/su/sa1zer/diversemodlib/utils/TimeUtils.java
@@ -0,0 +1,60 @@
+package su.sa1zer.diversemodlib.utils;
+
+import joptsimple.internal.Strings;
+import lombok.experimental.UtilityClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@UtilityClass
+public class TimeUtils {
+
+    public static String getDurationString(long duration, String[] arr) {
+        long diffSeconds = duration % 60;
+        long diffMinutes = duration / 60 % 60;
+        long diffHours = duration / (60 * 60) % 24;
+        long diffMonths = duration / (60 * 60 * 24) % 30;
+        long diffYears = duration / (60 * 60 * 24 * 30) % 12;
+
+        List<String> message = new ArrayList<>();
+
+        if(diffYears > 0) {
+            message.add(String.valueOf(diffYears));
+            message.add(" ");
+            message.add(arr[4]);
+        }
+        if(diffMonths > 0){
+            message.add(String.valueOf(diffMonths));
+            message.add(" ");
+            message.add(arr[3]);
+        }
+        if(diffHours > 0) {
+            message.add(String.valueOf(diffHours));
+            message.add(" ");
+            message.add(arr[2]);
+        }
+        if(diffMinutes > 0) {
+            message.add(String.valueOf(diffMinutes));
+            message.add(" ");
+            message.add(arr[1]);
+        }
+        if(diffSeconds > 0){
+            message.add(String.valueOf(diffSeconds));
+            message.add(" ");
+            message.add(arr[0]);
+        }
+
+        return Strings.join(message, " ");
+    }
+
+    public static String declOfNum(long n, String[] arr) {
+        n = n % 100;
+
+        long n1 = n % 10;
+        if (n > 10 && n < 20) return arr[2];
+        if (n1 > 1 && n1 < 5) return arr[1];
+        if (n1 == 1) return arr[0];
+
+        return n + " " +arr[2];
+    }
+}
